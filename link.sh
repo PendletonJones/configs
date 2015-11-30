ac243f542b445d473bef1eda58ae85c8b3faa27b
Pendleton Jones <pendletonmjones@gmail.com>
2015-11-30 17:14:25 -0500

    minor change

--- link.sh
+++ link.sh
@@ -2,3 +2,4 @@ ln -s ~/configs/.bash_aliases ~/.bash_aliases
 ln -s ~/configs/.bash_profile ~/.bash_profile 
 ln -s ~/configs/.profile ~/.profile 
 ln -s ~/configs/.vimrc ~/.vimrc 
+ln -s ~/configs/.bashrc ~/.bashrc
